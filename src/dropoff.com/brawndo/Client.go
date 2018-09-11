bebea21e5f606c7b612995006f027f9242ca5259
Algis Woss <awoss@dropoff.com>
2018-09-11 14:21:10 -0500

    * Support for simulation of real order in the sandbox

--- src/dropoff.com/brawndo/Client.go
+++ src/dropoff.com/brawndo/Client.go
@@ -18,7 +18,7 @@ func (b Client) Info() (InfoResponse, error) {
 	var ir InfoResponse
 	resp, err := b.Transport.MakeRequest("GET", "/info", "info", "", nil)
 
-	if (err != nil) {
+	if err != nil {
 		return ir, err
 	}
 
@@ -33,25 +33,25 @@ type AvailablePropertiesRequest struct {
 
 func (b Client) AvailableProperties(req *AvailablePropertiesRequest) (AvailablePropertiesResponse, error) {
 	var apgr AvailablePropertiesResponse
-	var query_string string
+	var queryString string
 
-	var company_id = req.CompanyId
+	var companyId = req.CompanyId
 
 	req.CompanyId = ""
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return apgr, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("GET", "/order/properties", "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("GET", "/order/properties", "order", queryString, nil)
 
 
-	if (err != nil) {
+	if err != nil {
 		return apgr, err
 	}
 
@@ -72,25 +72,25 @@ type GetSignatureResponse struct {
 
 func (b Client) GetSignature(req *GetSignatureRequest) (GetSignatureResponse, error) {
 	var gsr GetSignatureResponse
-	var query_string string
+	var queryString string
 
-	var company_id = req.CompanyId
+	var companyId = req.CompanyId
 
 	req.CompanyId = ""
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return gsr, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("GET", "/order/signature/" + req.OrderId, "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("GET", "/order/signature/" + req.OrderId, "order", queryString, nil)
 
 
-	if (err != nil) {
+	if err != nil {
 		return gsr, err
 	}
 
@@ -108,93 +108,93 @@ type EstimateRequest struct {
 }
 
 func (b Client) Estimate(req *EstimateRequest) (EstimateResponse, error) {
-	var origin = req.Origin;
-	var destination = req.Destination;
-	var ready_timestamp = req.ReadyTimestamp;
-	var utc_offset = req.UTCOffset;
-	var company_id = req.CompanyId;
+	var origin = req.Origin
+	var destination = req.Destination
+	var readyTimestamp = req.ReadyTimestamp
+	var utcOffest = req.UTCOffset
+	var companyId = req.CompanyId
 
 	var er EstimateResponse
 	query, err := url.ParseQuery("")
 
-	if (err != nil) {
+	if err != nil {
 		return er, err
 	}
 
-	query.Add("origin", origin);
-	query.Add("destination", destination);
+	query.Add("origin", origin)
+	query.Add("destination", destination)
 
-	if (company_id != "") {
-		query.Add("company_id", company_id);
+	if companyId != "" {
+		query.Add("company_id", companyId)
 	}
 
-	var utc_offset_string string
+	var utcOffsetString string
 
-	var utc_offset_hours = utc_offset / 3600
+	var utcOffsetHours = utcOffest / 3600
 
-	if (utc_offset_hours < 0) {
-		utc_offset_string = "-"
-		utc_offset_hours = utc_offset_hours * -1
+	if utcOffsetHours < 0 {
+		utcOffsetString = "-"
+		utcOffsetHours = utcOffsetHours * -1
 	}
 
-	if (utc_offset_hours < 10) {
-		utc_offset_string += "0"
+	if utcOffsetHours < 10 {
+		utcOffsetString += "0"
 	}
 
-	utc_offset_string += strconv.Itoa(utc_offset_hours) + ":00";
+	utcOffsetString += strconv.Itoa(utcOffsetHours) + ":00"
 
-	query.Add("utc_offset", utc_offset_string);
+	query.Add("utc_offset", utcOffsetString)
 
-	if (ready_timestamp > 0) {
-		query.Add("ready_timestamp", strconv.FormatInt(ready_timestamp, 10));
+	if readyTimestamp > 0 {
+		query.Add("ready_timestamp", strconv.FormatInt(readyTimestamp, 10))
 	}
 
 	resp, err := b.Transport.MakeRequest("GET", "/estimate", "estimate", "?" + query.Encode(), nil)
 
-	if (err != nil) {
+	if err != nil {
 		return er, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &er)
 
-	return er, nil;
+	return er, nil
 }
 
 func (b Client) CreateOrder(req *CreateOrderRequest) (CreateOrderResponse, error) {
 	var cor CreateOrderResponse
-	var query_string string
-	var company_id = req.CompanyId
+	var queryString string
+	var companyId = req.CompanyId
 
 	req.CompanyId = ""
 
 	ba, err := json.Marshal(req)
 
-	if (err != nil) {
+	if err != nil {
 		return cor, err
 	}
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return cor, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("POST", "/order", "order", query_string, ba)
+	resp, err := b.Transport.MakeRequest("POST", "/order", "order", queryString, ba)
 
-	if (err != nil) {
+	if err != nil {
 		return cor, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &cor)
 
-	if (err != nil) {
+	if err != nil {
 		return cor, err
 	}
 
-	return cor, nil;
+	return cor, nil
 }
 
 type OrderRequest struct {
@@ -206,120 +206,151 @@ type OrderRequest struct {
 func (b Client) GetOrder(req *OrderRequest) (GetOrderResponse, error) {
 	var gor GetOrderResponse
 
-	var order_id = req.OrderId;
-	var company_id = req.CompanyId;
+	var orderId = req.OrderId
+	var companyId = req.CompanyId
 
-	var query_string string
+	var queryString string
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return gor, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("GET", "/order/" + order_id, "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("GET", "/order/" + orderId, "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return gor, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &gor)
 
-	if (err != nil) {
+	if err != nil {
 		return gor, err
 	}
 
-	return gor, nil;
+	return gor, nil
 }
 
 func (b Client) GetOrderPage(req *OrderRequest) (GetOrdersResponse, error) {
 	var gor GetOrdersResponse
-	var query_string string
-	var last_key = req.LastKey;
-	var company_id = req.CompanyId;
+	var queryString string
+	var lastKey = req.LastKey
+	var companyId = req.CompanyId
 
-	if (last_key != "") {
+	if lastKey != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return gor, err
 		}
-		query.Add("last_key", last_key)
-		if (company_id != "") {
-			query.Add("company_id", company_id)
+		query.Add("last_key", lastKey)
+		if companyId != "" {
+			query.Add("company_id", companyId)
 		}
-		query_string = "?" + query.Encode()
-	} else if (company_id != "") {
+		queryString = "?" + query.Encode()
+	} else if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return gor, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("GET", "/order", "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("GET", "/order", "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return gor, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &gor)
 
-	if (err != nil) {
+	if err != nil {
 		return gor, err
 	}
 
-	return gor, nil;
+	return gor, nil
 }
 
 func (b Client) CancelOrder(req *OrderRequest) (CancelOrderResponse, error) {
 	var cor CancelOrderResponse
-	var query_string string
-	var order_id = req.OrderId;
-	var company_id = req.CompanyId;
+	var queryString string
+	var orderId = req.OrderId
+	var companyId = req.CompanyId
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return cor, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("POST", "/order/" + order_id + "/cancel", "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("POST", "/order/" +orderId+ "/cancel", "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return cor, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &cor)
 
-	if (err != nil) {
+	if err != nil {
 		return cor, err
 	}
 
-	return cor, nil;
+	return cor, nil
 }
 
-func (b Client) SimulateOrder(market string) (SimulateOrderResponse, error) {
+type SimulateRequest struct {
+	OrderId		string
+	Market		string
+	CompanyId   string
+}
+
+func (b Client) SimulateOrder(req *SimulateRequest) (SimulateOrderResponse, error) {
 	var sor SimulateOrderResponse
-	resp, err := b.Transport.MakeRequest("GET", "/order/simulate/" + market, "order", "", nil)
+	var queryString string
 
-	if (err != nil) {
-		return sor, err
+	if req.CompanyId != "" {
+		query, err := url.ParseQuery("")
+		if err != nil {
+			return sor, err
+		}
+		query.Add("company_id", req.CompanyId)
+		queryString = "?" + query.Encode()
 	}
 
-	err = json.Unmarshal([]byte(resp), &sor)
+	if req.OrderId != "" {
+		resp, err := b.Transport.MakeRequest("GET", "/order/simulate/order/" + req.OrderId, "order", queryString, nil)
 
-	if (err != nil) {
-		return sor, err
+		if err != nil {
+			return sor, err
+		}
+
+		err = json.Unmarshal([]byte(resp), &sor)
+
+		if err != nil {
+			return sor, err
+		}
+	} else if req.Market != "" {
+		resp, err := b.Transport.MakeRequest("GET", "/order/simulate/" + req.Market, "order", queryString, nil)
+
+		if err != nil {
+			return sor, err
+		}
+
+		err = json.Unmarshal([]byte(resp), &sor)
+
+		if err != nil {
+			return sor, err
+		}
 	}
 
-	return sor, nil;
+	return sor, nil
 }
 
 
@@ -332,94 +363,94 @@ type OrderTipRequest struct {
 func (b Client) CreateOrderTip(req *OrderTipRequest) (TipResponse, error) {
 	var tr TipResponse
 
-	var query_string string
+	var queryString string
 
-	var order_id = req.OrderId;
-	var amount = req.Amount;
-	var company_id = req.CompanyId;
+	var orderId = req.OrderId
+	var amount = req.Amount
+	var companyId = req.CompanyId
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return tr, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("POST", "/order/" + order_id + "/tip/" + amount, "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("POST", "/order/" +orderId+ "/tip/" + amount, "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &tr)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
-	return tr, nil;
+	return tr, nil
 }
 
 func (b Client) GetOrderTip(req *OrderTipRequest) (GetTipResponse, error) {
 	var tr GetTipResponse
 
-	var query_string string
-	var order_id = req.OrderId;
-	var company_id = req.CompanyId;
+	var queryString string
+	var orderId = req.OrderId
+	var companyId = req.CompanyId
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return tr, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("GET", "/order/" + order_id + "/tip", "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("GET", "/order/" +orderId+ "/tip", "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &tr)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
-	return tr, nil;
+	return tr, nil
 }
 
 func (b Client) DeleteOrderTip(req *OrderTipRequest) (DeleteTipResponse, error) {
 	var tr DeleteTipResponse
 
-	var query_string string
-	var order_id = req.OrderId;
-	var company_id = req.CompanyId;
+	var queryString string
+	var orderId = req.OrderId
+	var companyId = req.CompanyId
 
-	if (company_id != "") {
+	if companyId != "" {
 		query, err := url.ParseQuery("")
-		if (err != nil) {
+		if err != nil {
 			return tr, err
 		}
-		query.Add("company_id", company_id)
-		query_string = "?" + query.Encode()
+		query.Add("company_id", companyId)
+		queryString = "?" + query.Encode()
 	}
 
-	resp, err := b.Transport.MakeRequest("DELETE", "/order/" + order_id + "/tip", "order", query_string, nil)
+	resp, err := b.Transport.MakeRequest("DELETE", "/order/" +orderId+ "/tip", "order", queryString, nil)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
 	err = json.Unmarshal([]byte(resp), &tr)
 
-	if (err != nil) {
+	if err != nil {
 		return tr, err
 	}
 
-	return tr, nil;
+	return tr, nil
 }
