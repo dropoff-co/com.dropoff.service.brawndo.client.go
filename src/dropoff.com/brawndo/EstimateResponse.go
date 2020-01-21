abd0b644c3471c47636c61ad0c34066d6431374a
Algis Woss <awoss@dropoff.com>
2020-01-21 13:01:13 -0600

    Added coordinate support on the estimate call

--- src/dropoff.com/brawndo/EstimateResponse.go
+++ src/dropoff.com/brawndo/EstimateResponse.go
@@ -1,5 +1,16 @@
 package brawndo
 
+type EstimateLocation struct {
+	Lat				float64 `json:"lat"`
+	Lng				float64 `json:"lng"`
+	LocationType	string  `json:"type"`
+}
+
+type EstimateCoordinates struct {
+	Destination		*EstimateLocation			`json:"destination"`
+	Pickup			*EstimateLocation			`json:"pickup"`
+}
+
 type EstimateServiceType struct {
 	ETA 		string			`json:"ETA"`
 	Distance 	string			`json:"Distance"`
@@ -9,10 +20,12 @@ type EstimateServiceType struct {
 type EstimateData struct {
 	ETA 		string			`json:"ETA"`
 	Distance 	string			`json:"Distance"`
-	ServiceType 	string			`json:"service_type"`
+	ServiceType string			`json:"service_type"`
 	Asap  		*EstimateServiceType	`json:"asap"`
 	TwoHr  		*EstimateServiceType	`json:"two_hr"`
 	FourHr 		*EstimateServiceType	`json:"four_hr"`
+	AllDay 		*EstimateServiceType	`json:"all_day"`
+	Coordinates *EstimateCoordinates	`json:"coordinates"`
 }
 
 type EstimateResponse struct {
